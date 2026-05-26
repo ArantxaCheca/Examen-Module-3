139fbaea53ce2e239f53c21f280cbdf4cbdfbeb5
ArantxaCheca <checaarantxa6@gmail.com>
2026-05-26 11:49:44 +0200

    Add controller instantiation tests

--- src/back/products.controller.test.ts
+++ src/back/products.controller.test.ts
@@ -1,1 +1,24 @@
-// TESTS
+import { describe, expect, it, vi } from 'vitest';
+import { ProductsController } from './products.controller';
+
+describe('Given ProductsController', () => {
+    const mockRepo = {
+        read: vi.fn(),
+        readById: vi.fn(),
+        create: vi.fn(),
+        update: vi.fn(),
+        delete: vi.fn(),
+    };
+
+    const controller = new ProductsController(mockRepo);
+
+    describe('When instantiate controller', () => {
+        it('Then should exist', () => {
+            expect(controller).toBeDefined();
+        });
+
+        it('Then should be instance of ProductsController', () => {
+            expect(controller).toBeInstanceOf(ProductsController);
+        });
+    });
+});
